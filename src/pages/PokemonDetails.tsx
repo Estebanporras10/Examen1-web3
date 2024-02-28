c84acc375592ddf5b1d6a17860e34ea439a3c58f
Estebanporras10 <125413141+Estebanporras10@users.noreply.github.com>
2024-02-27 19:12:57 -0600

    Initial commit

---
+++ src/pages/PokemonDetails.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/modal.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  children: React.ReactNode;
+}
+
+const PokemonDetails: React.FC<ModalProps> = ({ isOpen, closeModal, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={closeModal}>
+      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close-button" onClick={closeModal}></button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default PokemonDetails;
